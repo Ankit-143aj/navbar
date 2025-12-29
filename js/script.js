346a53aa614acb5b3a081ac23204fc7cad53544a
Ankit-143aj <sankit9127@gmail.com>
2025-12-29 19:06:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,20 @@ fetch("navbar.html")
 .then(res => res.text())
 .then(data => {
   document.getElementById("navbar").innerHTML = data;
+
+  let currentPage = window.location.pathname.split("/").pop();
+  //menu me anchor tak pahucne ke liye
+  let links = document.querySelectorAll(".menu .menubar ul li a");
+
+  if(!currentPage || currentPage == "#"){
+    currentPage = "index.html";
+  }
+
+  links.forEach(link => {
+    let linkPage = link.getAttribute("href");
+
+    if(linkPage == currentPage){
+      link.closest("li").classList.add("underline_current"):
+    }
+  })
 });
