dae72804a2c2c98584a8e9f2ec2df214f187d016
Ankit-143aj <sankit9127@gmail.com>
2026-01-05 12:23:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,5 +17,26 @@ fetch("navbar.html")
     if(linkPage == currentPage){
       link.closest("li").classList.add("underline_current");
     }
+
+    if (window.innerWidth <= 576){
+      const Menu  =  document.querySelector(".menu");
+      const Menubar = document.querySelector(".menu .menubar ul");
+      const Mobilebar = document.querySelector(".menu .mobilebar");
+
+          if(Mobilebar && !Mobilebar.querySelector(".hamburger")){
+            const dv = document.createElement("div");
+            dv.className = "hamburger";
+            dv.setAttribute("aria-label","Toggle menu");
+            dv.setAttribute("tabindex","0");
+            dv.innerHTML = "<span></span>";
+            Mobilebar.appendChild(dv);
+    
+    
+            dv.addEventListner("click",function(e){
+              e.stopPropagation();
+              Menu.classList.toggle("mobile-open");
+            });
+      }
+    }
   })
 });
