b0f3f8dcc1a893cae8350df3101f70b56f65ae95
Ankit-143aj <sankit9127@gmail.com>
2026-01-06 12:56:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,77 +1,82 @@
-  fetch("navbar.html")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("navbar").innerHTML = data;
-  
+fetch("navbar.html")
+.then(res => res.text())
+.then(data => {
+	document.getElementById("navbar").innerHTML = data;
     let currentPage = window.location.pathname.split("/").pop();
-    //menu me anchor tak pahucne ke liye
-    if(!currentPage || currentPage == "#"){
-      currentPage = "index.html";
+    if ( !currentPage || currentPage == "#" ) {
+		currentPage = "index.html";
     }
-
+    // menu me anchor tak pahuchne ka tareeka
     let links = document.querySelectorAll(".menu .menubar ul li a");
+        
+	// hr anchor tag ko process karenge
     links.forEach(link => {
-      let linkPage = link.getAttribute("href");
-      if(linkPage == currentPage){
-        link.closest("li").classList.add("underline_current");
-      }
+		//pehle  anchor tag ka href ki value save karenge
+    	let linkPage = link.getAttribute("href");
+		
+        if(!linkPage) return;
+  
+        if (linkPage == currentPage) {
+      		link.closest("li").classList.add("underline_current");
+    	}
 
-      if (window.innerWidth <= 576){
-              const Menu  =  document.querySelector(".menu");
-              const Menubar = document.querySelector(".menu .menubar ul");
-              const Mobilebar = document.querySelector(".menu .mobilebar");
-              
-                  if(Mobilebar && !Mobilebar.querySelector(".hamburger")){
-                    const dv = document.createElement("div");
-                    dv.className = "hamburger";
-                    dv.setAttribute("aria-label","Toggle menu");
-                    dv.setAttribute("tabindex","0");
-                    dv.innerHTML = "<span></span>";
-                    Mobilebar.appendChild(dv);
-            
-                    dv.addEventListener("click",function(e){
-                      e.stopPropagation();
-                      Menu.classList.toggle("mobile-open");
-                    });
-       }
-      
-      // sabse pehle --> menu items nikalna padega
-      const menuList = document.querySelectorAll(".menu .menubar ul li");
-      menuList.forEach(li => {
-        // second step --> woh menu items filter karna jisme submenu ho
-        const submenuExists = li.querySelectorAll(".submenu");
-        if(submenuExists.length>0){
-          li.classList.add("has-submenu");
-          //third step--> identify karna jis menu item pr click hua ho
-          if(!li.querySelector(".submenu-toggle")){
-            const toggle = document.createElement("button");
-            toggle.className = "submenu-toggle";
-            toggle.setAttribute("aria-expanded","false");
-            toggle.innerHHTML = '<span style="display:inline-block; transform:rotate(90deg);">&#9656</span>';
+		if (window.innerWidth <= 576) {
+			const menu = document.querySelector(".menu");
+			const menuBar = document.querySelector(".menu .menubar ul");
+			const mobileBar = document.querySelector(".menu .mobilebar");
 
-            //dummy li add in the end of the list
-            const firstLink = li.querySelector("a");
-            firstLink.after(toggle);
+			if(mobileBar && !mobileBar.querySelector(".hamburger")) {
+				const dv = document.createElement("div");
+				dv.className = "hamburger";
+				dv.setAttribute("aria-label", "Toggle menu");
+				dv.setAttribute("tabindex","0");
+				dv.innerHTML= '<span></span>';
+				mobileBar.appendChild(dv);
 
-            //fourth Step ==> close already open/close all window
-            li.addEventListener("click", function(ev)){
-              ev.stopPropagation();
-              menuList.forEach(s_li => {
-                const s_submenuExists = s_li.querySelector(".submenu");
-                if(s_submenuExists && s_li !== li){
-                  console.log(s_submenuExists);
-                  console.log(s_li);
-                  s_li.classList.remove("open-submenu");
-                }
-              });
-              //fifth step ==> submenu display karna jo menu item click hua ho
-              const isOpen = li.classList.toggle("open-submenu");
-              toggle.setAttribute("aria-expanded",isOpen? "true" : "false");
-            });
-            
-          }
-        }
-      });
-    }
-  });
-});
+				dv.addEventListener("click", function(e) {
+					e.stopPropagation();
+					menu.classList.toggle("mobile-open");
+				});
+			}
+
+			// sabse pehle --> menu items nikalne padenge
+			const menuList = document.querySelectorAll(".menu .menubar ul li");
+			
+			menuList.forEach(li => {
+				// second step --> voh menu items filter karna jisme submenu ho
+				const submenuExists = li.querySelectorAll(".submenu"); // will return undefined or actual value node
+				// console.log(submenuExists);
+				if (submenuExists.length>0) {
+					li.classList.add("has-submenu");
+					// third step --> identify karna jis menu item pr click hua ho
+					if(!li.querySelector(".submenu-toggle")) {
+						const toggle = document.createElement("button");
+						toggle.className = "submenu-toggle";
+						toggle.setAttribute("aria-expanded", "false");
+						toggle.innerHTML = '<span style="display:inline-block; transform:rotate(90deeg);">&#9656;</span>';
+	
+						// dummy li add in the end of the list
+						const firstLink = li.querySelector("a");
+						firstLink.after(toggle);
+		
+						// fouth step --> close already open / close all submenu
+						li.addEventListener("click", function (ev) {
+							ev.stopPropagation();
+							menuList.forEach(s_li =>{
+								const s_submenuExists = s_li.querySelector(".submenu");
+								if(s_submenuExists && s_li !== li){
+									console.log(s_submenuExists);
+									console.log(s_li);
+									s_li.classList.remove("open-submenu");
+								}
+							});
+							// fifth step --> submenu display karna jo menu item click hua ho
+							const isOpen = li.classList.toggle("open-submenu");
+							toggle.setAttribute("aria-expanded", isOpen? "true" : "false");
+						});
+					}
+				}
+			});
+		}
+	});
+})
