2b4b4dd7ae85b4b90aede4e0eab7cacd20497656
Ankit-143aj <sankit9127@gmail.com>
2026-01-15 09:59:47 +0530

    Create form.js

---
+++ js/form.js
@@ -0,0 +1,11 @@
+const supabaseClient = window.supabase.createClient(
+  "https://lqoyadmxuywcgmgdigdl.supabase.co"
+  "sb_publishable_litH-Xgde6TYyFXoMycjTA_az--2eXV "
+);
+
+if (supabaseClient){
+  console.log("Database connected:",supabaseClient);
+}
+else{
+  console.log("Database not created");
+}
