4bebcfda1d3f1f99c0614c5d7c53a6930cfa3e0f
Ankit-143aj <sankit9127@gmail.com>
2026-01-15 10:12:05 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
 const supabaseClient = window.supabase.createClient(
-  "https://lqoyadmxuywcgmgdigdl.supabase.co"
+  "https://lqoyadmxuywcgmgdigdl.supabase.co",
   "sb_publishable_litH-Xgde6TYyFXoMycjTA_az--2eXV "
 );
 
